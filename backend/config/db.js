a0c8e8a69353c592dbfc4f52151b289d87118008
moetez96 <moetez.ayari@esprit.tn>
2021-12-11 20:38:35 +0100

    adding the database

---
+++ backend/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://moetez:ayari1996@cluster0.lteno.mongodb.net/testdb?retryWrites=true&w=majority"
+}
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./config/db.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose.Promise = global.Promise;
+mongoose.connect(db.url, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to DB');
+    }).catch(err => {
+        console.log(err.message);
+    });
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Connected at : ' + port);
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
